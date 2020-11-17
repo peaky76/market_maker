4928e62b4c445fc79af0b2ddb50965e7fa3e35c1
peaky76 <robertjamespeacock@gmail.com>
2020-11-17 15:05:52 +0000

    Switched to pytest

--- tests/test_bet.py
+++ tests/test_bet.py
@@ -2,18 +2,12 @@ import sys
 sys.path.append('..')
 
 from bet import Bet
-import unittest
 
-class TestBet(unittest.TestCase):
+def test_kelly_positive():
+    assert Bet.kelly(0.5, 3.0, 100) == 25
 
-    def test_kelly_positive(self):
-        self.assertEqual(Bet.kelly(0.5, 3.0, 100), 25)
-
-    def test_kelly_zero(self):
-        self.assertEqual(Bet.kelly(0.5, 2.0, 100), 0)
-
-    def test_kelly_negative(self):
-        self.assertEqual(Bet.kelly(0.5, 1.5, 100), 0)        
-        
-if __name__ == '__main__':
-    unittest.main()
+def test_kelly_zero():
+    assert Bet.kelly(0.5, 2.0, 100) == 0
+    
+def test_kelly_negative():
+    assert Bet.kelly(0.5, 1.5, 100) == 0       
--- tests/test_odds.py
+++ tests/test_odds.py
@@ -2,15 +2,9 @@ import sys
 sys.path.append('..')
 
 from odds import Odds
-import unittest
 
-class TestOdds(unittest.TestCase):
+def test_from_decimal():
+    assert Odds(3).decimal == 3.0
 
-    def test_from_decimal(self):
-        self.assertEqual(Odds(3).decimal, 3.0)
-
-    def test_from_fractional(self):
-        self.assertEqual(Odds.from_fractional(5, 2).decimal, 3.5)        
-        
-if __name__ == '__main__':
-    unittest.main()
+def test_from_fractional():
+    assert Odds.from_fractional(5, 2).decimal == 3.5        
